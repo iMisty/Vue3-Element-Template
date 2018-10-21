e7e58851c89a2a3cecabce564e80f02dfff81550
Miya <qianwumeng_2cyndi@qq.com>
2018-10-21 18:31:19 +0800

    Update src/components/header.vue

---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="primary-title">{{primary}}</h1>
+    <h2 class="secend-title">{{secend}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      primary: 'Landing Page',
+      secend: '点击以下链接开始'
+    }
+  }
+}
+</script>
+<style>
+h1,h2{
+  transition: .2s ease;
+  letter-spacing: 4px;
+  padding: 8px 0;
+  text-align: center;
+}
+h1{
+  font-size: 8vw;
+}
+h2{
+  font-size: 4vw;
+}
+</style>
