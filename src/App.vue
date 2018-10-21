c365038be6c9f47b1ee8180cd149f32d4098e0a0
Miya <qianwumeng_2cyndi@qq.com>
2018-10-21 18:31:15 +0800

    Update src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Links></Links>
+    <p id="hitokoto">加载中...</p>
+  </div>
+</template>
+
+<script>
+import header from './components/header'
+import Links from './components/links'
+import './assets/global.css'
+
+export default {
+  name: 'App',
+  components: {
+    'Header': header,
+    'Links': Links
+  }
+}
+</script>
+
+<style>
+#app{
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: rgba(0,0,0,.4);
+}
+#hitokoto{
+  text-align: center;
+}
+</style>
