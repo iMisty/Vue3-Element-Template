5fd1778715493bd9d08c2a2fad1618419085f817
Miya <qianwumeng_2cyndi@qq.com>
2018-10-21 18:31:22 +0800

    Update src/components/links.vue

---
+++ src/components/links.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <ul>
+    <li v-for="item of list" :key="item.index">
+      <a :href="item.link">
+        <span>{{ item.value }}</span>
+      </a>
+    </li>
+  </ul>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      list: [
+        {
+          link: 'https://github.com/Miya-Yukarin/Vue-LandingPage',
+          value: 'GitHub'
+        },
+        {
+          link: 'https://github.com/Miya-Yukarin/Vue-LandingPage/blob/master/README.md',
+          value: 'Manual'
+        },
+        {
+          link: 'https://shizu17.pw/old',
+          value: 'Another Demo'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+ul {
+  padding: 0;
+  list-style: none;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+}
+li {
+  list-style: none;
+  font-size: 2.2vw;
+  padding: 8px 40px 40px 0;
+  transition: 0.2s ease;
+  text-align: center;
+}
+span {
+  padding: 0 8px;
+}
+a:hover {
+  background-color: #fff;
+  color: #000;
+  transition: 0.2s ease;
+}
+</style>
