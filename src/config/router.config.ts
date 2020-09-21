ca3f9eb7836aee6fabc7c95ead476460e4e663f7
Miya <qianwumeng_2cyndi@qq.com>
2020-09-22 00:59:36 +0800

    Update: Change Logo

--- src/config/router.config.ts
+++ src/config/router.config.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-21 18:17:43
- * @LastEditTime: 2020-09-21 18:36:38
+ * @LastEditTime: 2020-09-22 00:54:47
  * @LastEditors: Miya
  * @Description: 导航栏配置
- * @FilePath: \LandingPage\src\config\router.config.ts
+ * @FilePath: /Kagura-LandingPage/src/config/router.config.ts
  * @Version: 1.0
  */
 interface Route {
@@ -24,23 +24,23 @@ export const Router: Route[] = [
     icon: '',
     text: '项目',
     link: '/work',
-    child: [
-      {
-        icon: '',
-        text: '项目1',
-        link: '/1'
-      },
-      {
-        icon: '',
-        text: '项目2',
-        link: '/2'
-      },
-      {
-        icon: '',
-        text: '项目3',
-        link: '/3'
-      }
-    ]
+    // child: [
+    //   {
+    //     icon: '',
+    //     text: '项目1',
+    //     link: '/1'
+    //   },
+    //   {
+    //     icon: '',
+    //     text: '项目2',
+    //     link: '/2'
+    //   },
+    //   {
+    //     icon: '',
+    //     text: '项目3',
+    //     link: '/3'
+    //   }
+    // ]
   },
   {
     icon: '',
