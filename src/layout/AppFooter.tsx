34754d7550af8b6d9405af113b301b7d9da9c8c6
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-09-23 18:35:21 +0800

    Create: App footer

---
+++ src/layout/AppFooter.tsx
@@ -0,0 +1,13 @@
+import { Component, Vue } from 'vue-property-decorator';
+
+
+@Component({})
+export default class AppFooter extends Vue {
+
+
+  private render() {
+    return (
+      <div class="app__footer"></div>
+    );
+  }
+}
