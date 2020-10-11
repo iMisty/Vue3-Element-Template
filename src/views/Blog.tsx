545d68351ddfa0c76d78762a371aaba3fa85ac76
Miya <qianwumeng_2cyndi@qq.com>
2020-10-12 01:30:56 +0800

    Update: 文章页面框架

---
+++ src/views/Blog.tsx
@@ -0,0 +1,7 @@
+import { Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class Blog extends Vue {
+  private render() {
+    return <div class="blog"></div>;
+  }
+}
---
+++ src/views/BlogWrap.tsx
@@ -0,0 +1,7 @@
+import { Component, Vue } from 'vue-property-decorator';
+@Component({})
+export default class BlogWrap extends Vue {
+  private render() {
+    return <div class="blog__wrap"></div>;
+  }
+}
