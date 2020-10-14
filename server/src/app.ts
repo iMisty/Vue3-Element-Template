5fb2cacedbced869a399ab1c337f4e4f633445f6
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-10-14 18:45:58 +0800

    init

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,34 +1,17 @@
 /*
  * @Author: Miya
  * @Date: 2020-10-14 16:44:35
- * @LastEditTime: 2020-10-14 17:02:46
+ * @LastEditTime: 2020-10-14 18:45:37
  * @LastEditors: Miya
  * @Description: Koa APP
  * @FilePath: \LandingPage\server\src\app.ts
  * @Version: 1.0
  */
 import * as Koa from 'Koa';
-import * as Router from 'koa-router';
 import mongoose = require('mongoose');
-import { dbs } from './config/db';
 
 const app = new Koa();
-const router = new Router();
-const port = 12451;
-
-// Router
-router.get('/', async (ctx, next) => {
-  ctx.body = 'Hello TypeScript';
-});
-
-// MiddleWare
-app.use(router.routes());
-
-// MongoDB
-mongoose.connect(dbs, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const port = 12459;
 
 app.listen(port);
 
