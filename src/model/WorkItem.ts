521854dc7d5da28555516bc0483db487ad52c0cc
Miya <qianwumeng_2cyndi@qq.com>
2020-11-04 02:16:56 +0800

    Update: 字段注释

--- src/model/WorkItem.ts
+++ src/model/WorkItem.ts
@@ -3,21 +3,36 @@
  * @Version: 1.0
  * @Date: 2020-10-11 03:38:36
  * @LastEditors: Miya
- * @LastEditTime: 2020-11-03 17:48:20
+ * @LastEditTime: 2020-11-04 00:34:18
  * @Description: 项目表示接口
- * @FilePath: \LandingPage\src\model\WorkItem.ts
+ * @FilePath: /Kagura-Landing-Backend/Users/linjingyi/Documents/GitHub/Kagura-LandingPage/src/model/WorkItem.ts
  */
+
 export interface WorkItem {
+  // mongodb 自带 id
   _id: string;
+  // 用户设置 ID
   id?: number;
-  avatar?: object | string;
+  // 头像
+  avatar?: string;
+  // 标题
   title?: string;
+  // 标签
   tag?: string[];
+  // 简介
   intro?: string;
+  // 内文
   content?: string;
+  // 演示地址
   preview?: string;
+  // 源代码仓库
   source: string;
+  // 最后更新日期
   lastupdate?: string | number;
+  // 版本
   version?: string;
+  // 状态
   status?: string;
+  // mongodb 自带版本控制
+  __v?: string;
 }
