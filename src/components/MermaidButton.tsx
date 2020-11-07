e3b87535a7a669d62bbda293a12c97befbeedc38
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-11-07 15:37:24 +0800

    Update: Button Type
    - 增加按钮样式：普通、幽灵、纯文字

--- src/components/MermaidButton.tsx
+++ src/components/MermaidButton.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-03 17:08:06
- * @LastEditTime: 2020-10-27 17:42:39
+ * @LastEditTime: 2020-11-09 10:58:31
  * @LastEditors: Miya
  * @Description: Button component in Mermaid UI
  * @FilePath: \LandingPage\src\components\MermaidButton.tsx
@@ -10,11 +10,18 @@
 import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
 @Component({})
 export default class MermaidButton extends Vue {
-
   // Button Color
   // 按钮颜色
   @Prop({ default: 'primary' })
   private color!: string;
+
+  // Button Type
+  @Prop({ default: 'round' })
+  private type!: string;
+
+  // Button Size
+  @Prop({ default: 'regular' })
+  private size!: string;
 
   // Click Event
   // 点击事件
@@ -26,7 +33,7 @@ export default class MermaidButton extends Vue {
   private render() {
     return (
       <button
-        class={`mermaid__button mermaid__button--${this.color}`}
+        class={`mermaid__button mermaid__button--${this.color} mermaid__button--type-${this.type}-${this.color}`}
         onClick={this.handleClick}
       >
         {this.$slots.default}
