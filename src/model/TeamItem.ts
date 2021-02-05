7cb22bbfc3ae638d4b0d019c2e9034ab82aa4eba
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-02-05 17:48:39 +0800

    Update TeamItem.ts

--- src/model/TeamItem.ts
+++ src/model/TeamItem.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2020-11-05 17:51:38
- * @LastEditTime: 2020-11-05 17:53:45
+ * @LastEditTime: 2021-02-05 12:23:16
  * @LastEditors: Miya
  * @Description: TeamItem interface
  * @FilePath: \LandingPage\src\model\TeamItem.ts
@@ -12,5 +12,5 @@ export interface TeamItem{
   title?: string;
   content?: string;
   __v?: number;
-  _id: string;
+  _id?: string;
 }
