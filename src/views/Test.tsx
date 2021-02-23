a9437fbcd703939e1585740e11adddb8e818769d
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2021-02-23 16:50:00 +0800

    Update Test.tsx

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,5 +1,15 @@
+/*
+ * @Author: Miya
+ * @Date: 2020-10-22 14:58:55
+ * @LastEditTime: 2021-02-23 16:46:57
+ * @LastEditors: Miya
+ * @Description:
+ * @FilePath: \LandingPage\src\views\Test.tsx
+ * @Version:
+ */
 import { Component, Vue } from 'vue-property-decorator';
 import MermaidModel from '@/components/MermaidModel';
+import { AJAX } from '@/util/ajax';
 @Component({
   components: {
     'm-model': MermaidModel
@@ -10,21 +20,9 @@ export default class Test extends Vue {
 
   private aa = false;
 
-  private aaa() {
-    return this.$model({
-      title: 'a',
-      msg: 'aaa',
-      btn: {
-        confirm: 'affffff',
-        cancel: 'ffffb'
-      }
-    })
-      .then(res => {
-        console.log('yes', res);
-      })
-      .catch(rej => {
-        console.log('no', rej);
-      });
+  private async aaa() {
+    const a = await AJAX.init('get', 'https://v2.dressweb.cn/info/blog');
+    console.log(a);
   }
 
   private add() {
@@ -37,7 +35,7 @@ export default class Test extends Vue {
         class="test"
         style="width:100vw;height:100vh;magrin-top: 4rem;display:flex;flex-direction:column;justify-content:center;align-items:center;"
       >
-        {/* {this.aa ? <m-model onClose={() => this.add()}></m-model> : ''} */}
+        {this.aa ? <m-model onClose={() => this.add()}></m-model> : ''}
         <button onClick={() => this.aaa()}>1111111111</button>
       </div>
     );
