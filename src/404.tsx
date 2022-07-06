e84f7dc85a78016ee02aea3b9d8c4e2987dbc33f
Kagura Koeri <16733568+iMisty@users.noreply.github.com>
2022-07-06 18:02:37 +0800

    :truck: vue → tsx

--- src/404.tsx
+++ src/404.tsx
@@ -3,11 +3,11 @@
  * @Version: 2.0
  * @Author: Mirage
  * @Date: 2022-06-30 17:32:29
- * @LastEditors: Miya
- * @LastEditTime: 2022-06-30 22:57:49
+ * @LastEditors: Mirage
+ * @LastEditTime: 2022-07-06 17:23:59
  */
 import { FunctionalComponent, computed } from 'vue';
-import '@/style/layout/404.less';
+import Style from '@/style/layout/404.module.less';
 
 const computedMessage = computed(() => {
   return 'The webmaster said that you can not enter this page...';
@@ -15,33 +15,33 @@ const computedMessage = computed(() => {
 
 const Page404: FunctionalComponent = () => {
   return (
-    <div class="wscn-http404-container">
-      <div class="wscn-http404">
-        <div class="pic-404">
+    <div class={Style['wscn-http404-container']}>
+      <div class={Style['wscn-http404']}>
+        <div class={Style['pic-404']}>
           <img
-            class="pic-404__parent"
+            class={Style['pic-404__parent']}
             src="/src/assets/404_images/404.png"
             alt="404"
           />
           <img
-            class="pic-404__child left"
+            class={`${Style['pic-404__child']} ${Style['left']}`}
             src="/src/assets/404_images/404_cloud.png"
             alt="404"
           />
           <img
-            class="pic-404__child mid"
+            class={`${Style['pic-404__child']} ${Style['mid']}`}
             src="/src/assets/404_images/404_cloud.png"
             alt="404"
           />
           <img
-            class="pic-404__child right"
+            class={`${Style['pic-404__child']} ${Style['right']}`}
             src="/src/assets/404_images/404_cloud.png"
             alt="404"
           />
         </div>
-        <div class="bullshit">
-          <div class="bullshit__oops">OOPS!</div>
-          <div class="bullshit__info">
+        <div class={Style['bullshit']}>
+          <div class={Style['bullshit__oops']}>OOPS!</div>
+          <div class={Style['bullshit__info']}>
             All rights reserved
             <a
               style="color: #20a0ff"
@@ -51,12 +51,12 @@ const Page404: FunctionalComponent = () => {
               wallstreetcn
             </a>
           </div>
-          <div class="bullshit__headline">{{ computedMessage }}</div>
-          <div class="bullshit__info">
+          <div class={Style['bullshit__headline']}>{{ computedMessage }}</div>
+          <div class={Style['bullshit__info']}>
             Please check that the URL you entered is correct, or click the
             button below to return to the homepage.
           </div>
-          <a href="/" class="bullshit__return-home">
+          <a href="/" class={Style['bullshit__return-home']}>
             Back to home
           </a>
         </div>
---
+++ src/components/NavBarHamburger/index.tsx
@@ -0,0 +1,45 @@
+/*
+ * @Description: Hamburger Component for Navbar
+ * @Version: 2.0
+ * @Author: Mirage
+ * @Date: 2021-11-28 03:07:28
+ * @LastEditors: Mirage
+ * @LastEditTime: 2022-07-06 16:32:07
+ */
+import { defineComponent, computed, ComputedRef } from 'vue';
+import { useAPPStore } from '@/store/appStore';
+import Style from '@/style/layout/navbar/hamburger.module.less';
+
+const handleClickToggleNavbarStatus = () => {
+  const store = useAPPStore();
+  store.toggleNavBarCollapse();
+};
+
+const NavbarHamburger = defineComponent({
+  name: 'NavBarHamburger',
+  setup() {
+    const isActive: ComputedRef<boolean> = computed<boolean>(() => {
+      const store = useAPPStore();
+      return store.isCollapse;
+    });
+    return { isActive, handleClickToggleNavbarStatus };
+  },
+  render() {
+    return (
+      <div
+        class={Style['layout__navbar--hamburger']}
+        onClick={() => handleClickToggleNavbarStatus()}
+      >
+        <svg
+          class={[Style['hamburger'], this.isActive ? Style['is-active'] : '']}
+          viewBox="0 0 1024 1024"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 0 0 0-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0 0 14.4 7z" />
+        </svg>
+      </div>
+    );
+  },
+});
+
+export default NavbarHamburger;
