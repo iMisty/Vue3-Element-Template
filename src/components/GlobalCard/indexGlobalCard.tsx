1274a4c3f15a5d34f17f86d739e0f3f66b88e448
Kagura Koeri <16733568+iMisty@users.noreply.github.com>
2022-07-21 22:19:27 +0800

    :alarm_clock:  Template Page

---
+++ src/components/GlobalCard/indexGlobalCard.tsx
@@ -0,0 +1,34 @@
+/*
+ * @Description: Global Card Block Layout
+ * @Version: 1.0
+ * @Author: Mirage
+ * @Date: 2022-07-21 19:56:11
+ * @LastEditors: Mirage
+ * @LastEditTime: 2022-07-21 22:08:22
+ */
+
+import { FunctionalComponent } from 'vue';
+
+type CardProps = {
+  shadow: 'always' | 'hover' | 'never';
+};
+
+const GlobalCard: FunctionalComponent<CardProps> = (
+  props: CardProps,
+  context
+) => {
+  const { slots, emit } = context;
+
+  return (
+    <el-card shadow={(props.shadow = 'never')}>
+      {slots['header'] ? (
+        <section class="card__header">{slots['header']?.()}</section>
+      ) : null}
+      {slots['default'] ? (
+        <section class="card__container">{slots['default']?.()}</section>
+      ) : null}
+    </el-card>
+  );
+};
+
+export default GlobalCard;
--- src/views/example/child1.tsx
+++ src/views/example/child1.tsx
@@ -4,15 +4,26 @@
  * @Author: Mirage
  * @Date: 2022-07-04 17:17:17
  * @LastEditors: Mirage
- * @LastEditTime: 2022-07-04 17:18:45
+ * @LastEditTime: 2022-07-21 22:14:47
  */
 import { FunctionalComponent } from 'vue';
+import GlobalCard from '@/components/GlobalCard/indexGlobalCard';
 
 const exampleNest1: FunctionalComponent = () => {
   return (
-    <div>
-      <h1>This is Test 1-1-1</h1>
-    </div>
+    <el-space fill={true}>
+      <el-row>
+        <el-col span={4}>
+          <GlobalCard shadow="never">333333333333333333333</GlobalCard>
+        </el-col>
+        <el-col span={4}>
+          <GlobalCard shadow="never">333333333333333333333</GlobalCard>
+        </el-col>
+        <el-col span={16}>
+          <GlobalCard shadow="never">333333333333333333333</GlobalCard>
+        </el-col>
+      </el-row>
+    </el-space>
   );
 };
 
