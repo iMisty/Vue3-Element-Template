00874c6a9e267322b8303343b9f27a7927460bf8
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-08-11 00:11:02 +0800

    :zap: Add Global Router View

---
+++ src/views/RouterViews.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Description: Global Router View Component
+ * @Version: 2.0
+ * @Author: Mirage
+ * @Date: 2022-07-22 15:00:37
+ * @LastEditors: Miya
+ * @LastEditTime: 2022-08-11 00:10:10
+ */
+import { FunctionalComponent } from 'vue';
+
+const RouterView: FunctionalComponent = () => {
+  return <router-view></router-view>;
+};
+
+export default RouterView;
