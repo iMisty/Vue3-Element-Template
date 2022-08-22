8741caf32a55bc1af1660b7a21c0abaf81c947e0
Kagura Koeri <16733568+iMisty@users.noreply.github.com>
2022-08-22 18:15:42 +0800

    :construction: Require ECharts

--- src/utils/ECharts.ts
+++ src/utils/ECharts.ts
@@ -4,24 +4,30 @@
  * @Author: Mirage
  * @Date: 2022-08-19 11:33:16
  * @LastEditors: Mirage
- * @LastEditTime: 2022-08-19 16:01:09
+ * @LastEditTime: 2022-08-22 17:37:54
  */
 import * as echarts from 'echarts/core';
 import {
   TitleComponent,
-  TitleComponentOption,
   ToolboxComponent,
-  ToolboxComponentOption,
   TooltipComponent,
-  TooltipComponentOption,
   GridComponent,
-  GridComponentOption,
   LegendComponent,
-  LegendComponentOption,
 } from 'echarts/components';
-import { LineChart, LineSeriesOption } from 'echarts/charts';
+import { BarChart, LineChart } from 'echarts/charts';
 import { UniversalTransition } from 'echarts/features';
 import { CanvasRenderer } from 'echarts/renderers';
+import type {
+  BarSeriesOption,
+  LineSeriesOption,
+  TooltipComponentOption,
+  ToolboxComponentOption,
+  GridComponentOption,
+  TitleComponentOption,
+  LegendComponentOption,
+  XAXisComponentOption,
+  YAXisComponentOption,
+} from 'echarts';
 
 echarts.use([
   TitleComponent,
@@ -29,6 +35,7 @@ echarts.use([
   TooltipComponent,
   GridComponent,
   LegendComponent,
+  BarChart,
   LineChart,
   CanvasRenderer,
   UniversalTransition,
@@ -41,101 +48,38 @@ type EChartsOption = echarts.ComposeOption<
   | GridComponentOption
   | LegendComponentOption
   | LineSeriesOption
+  | BarSeriesOption
 >;
 
-// var chartDom = document.getElementById('main')!;
-// var myChart = echarts.init(chartDom);
-// var option: EChartsOption;
-
-// option = {
-//   title: {
-//     text: 'Stacked Line',
-//   },
-//   tooltip: {
-//     trigger: 'axis',
-//   },
-//   legend: {
-//     data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
-//   },
-//   grid: {
-//     left: '3%',
-//     right: '4%',
-//     bottom: '3%',
-//     containLabel: true,
-//   },
-//   toolbox: {
-//     feature: {
-//       saveAsImage: {},
-//     },
-//   },
-//   xAxis: {
-//     type: 'category',
-//     boundaryGap: false,
-//     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-//   },
-//   yAxis: {
-//     type: 'value',
-//   },
-//   series: [
-//     {
-//       name: 'Email',
-//       type: 'line',
-//       stack: 'Total',
-//       data: [120, 132, 101, 134, 90, 230, 210],
-//     },
-//     {
-//       name: 'Union Ads',
-//       type: 'line',
-//       stack: 'Total',
-//       data: [220, 182, 191, 234, 290, 330, 310],
-//     },
-//     {
-//       name: 'Video Ads',
-//       type: 'line',
-//       stack: 'Total',
-//       data: [150, 232, 201, 154, 190, 330, 410],
-//     },
-//     {
-//       name: 'Direct',
-//       type: 'line',
-//       stack: 'Total',
-//       data: [320, 332, 301, 334, 390, 330, 320],
-//     },
-//     {
-//       name: 'Search Engine',
-//       type: 'line',
-//       stack: 'Total',
-//       data: [820, 932, 901, 934, 1290, 1330, 1320],
-//     },
-//   ],
-// };
-
-// option && myChart.setOption(option);
+type ToolTipTrigger = 'axis' | 'item' | 'none';
 
 class ECharts {
+  /**
+   * @constructor
+   * Render Charts DOM HTML Element
+   */
   private renderDOM: HTMLElement;
 
+  /**
+   * ECharts Option
+   */
   private chartOptions: EChartsOption = {
     title: {
-      text: 'Stacked Line',
+      // text: 'Sample Charts',
     },
     tooltip: {
-      trigger: 'axis',
+      // trigger: 'axis',
     },
     legend: {
-      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+      // data: ['Sample Data'],
     },
     grid: {
       left: '4%',
       right: '4%',
       bottom: '4%',
-      containLabel: true,
+      // containLabel: true,
     },
-    toolbox: {
-      feature: {
-        saveAsImage: {},
-      },
-    },
+    toolbox: {},
     xAxis: {
       type: 'category',
       boundaryGap: false,
@@ -146,54 +90,249 @@ class ECharts {
     },
     series: [
       {
-        name: 'Email',
+        // name: 'Sample Data',
         type: 'line',
-        stack: 'Total',
+        // stack: 'Total',
+        smooth: true,
         data: [120, 132, 101, 134, 90, 230, 210],
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [220, 182, 191, 234, 290, 330, 310],
-      },
-      {
-        name: 'Video Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [150, 232, 201, 154, 190, 330, 410],
-      },
-      {
-        name: 'Direct',
-        type: 'line',
-        stack: 'Total',
-        data: [320, 332, 301, 334, 390, 330, 320],
-      },
-      {
-        name: 'Search Engine',
-        type: 'line',
-        stack: 'Total',
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
       },
     ],
   };
 
   private initChart: echarts.ECharts | undefined;
 
+  private resizeTimer: NodeJS.Timeout | null = null;
+
   constructor(dom: HTMLElement) {
     this.renderDOM = dom;
   }
 
+  /**
+   * Initial Chart and Set Render DOM
+   */
   private initialChart() {
     this.initChart = echarts.init(this.renderDOM);
   }
 
+  /**
+   *  @TODO Set SubClass
+   */
+
+  public getTitle() {
+    return this.chartOptions.title;
+  }
+
+  /**
+   * Set This Chart's Title
+   * @description If Title Param is String,Then only Set Base Title Options.
+   * Else Title is Object,Set Title Component Option
+   * @param title {TitleComponentOption | string} Title Options in this Chart
+   * @see https://echarts.apache.org/zh/option.html#title
+   */
+  public setTitle(title: TitleComponentOption | string) {
+    if (typeof title === 'string') {
+      return (this.chartOptions.title = {
+        text: title,
+      });
+    }
+    return (this.chartOptions.title = title);
+  }
+
+  /**
+   * Get This Charts Tooltip Option
+   * @returns Tooltip Options Data
+   */
+  public getTooltip() {
+    return this.chartOptions.tooltip;
+  }
+
+  /**
+   * Set Tooltip Component Option
+   * @description If tooltip Param is String,Then only Set Base Tooltip Options.
+   * Else Tooltip is Object,Set Tooltip Component Option
+   * @param tooltip {TooltipComponentOption | ToolTipTrigger} Tooltip Option
+   * @see https://echarts.apache.org/zh/option.html#tooltip
+   */
+  public setTooltip(tooltip: TooltipComponentOption | ToolTipTrigger) {
+    if (typeof tooltip === 'string') {
+      return (this.chartOptions.tooltip = {
+        trigger: tooltip,
+      });
+    }
+    return (this.chartOptions.tooltip = tooltip);
+  }
+
+  /**
+   * Get This Charts Legend Option
+   * @returns Legend Options Data
+   */
+  public getLegend() {
+    return this.chartOptions.legend;
+  }
+
+  /**
+   * Set Legend Component Option
+   * @description If Legend Param is Array,Then only Set Base Legend Options.
+   * Else Legend is Object,Set Legend Component Option
+   * @param legend {LegendComponentOption | LegendTrigger} Legend Option
+   * @see https://echarts.apache.org/zh/option.html#legend
+   */
+  public setLegend(
+    legend: LegendComponentOption | LegendComponentOption['data']
+  ) {
+    if (!legend) {
+      throw new Error('Legend is Required');
+    }
+    if (Array.isArray(legend)) {
+      return (this.chartOptions!.legend = {
+        data: legend,
+      });
+    }
+    return (this.chartOptions.legend = legend);
+  }
+
+  /**
+   * Get This Charts Grid Option
+   * @returns Grid Options Data
+   */
+  public getGrid() {
+    return this.chartOptions.grid;
+  }
+
+  /**
+   * Set Grid Component Option
+   * @description If Grid Param is null/undefined,Then only Set Base Grid Options.
+   * Else Grid is Object,Set Grid Component Option
+   * @default {left: '4%',right: '4%',bottom: '4%',containLabel: true}
+   * @param gridTheme {GridComponentOption | GridTrigger} Grid Option
+   * @see https://echarts.apache.org/zh/option.html#grid
+   */
+  public setGrid(gridTheme?: GridComponentOption | undefined) {
+    if (!gridTheme) {
+      return (this.chartOptions.grid = {
+        left: '4%',
+        right: '4%',
+        bottom: '4%',
+        containLabel: true,
+      });
+    }
+    return (this.chartOptions.grid = gridTheme);
+  }
+
+  /**
+   * Get This Charts Toolbox Option
+   * @returns Toolbox Options Data
+   */
+  public getToolbox() {
+    return this.chartOptions.toolbox;
+  }
+
+  /**
+   * Set Toolbox Component
+   * @description If Param Toolbox is null/undefined,Toolbox will be removed
+   * @param toolbox {ToolboxComponentOption | undefined} Toolbox Config
+   * @see https://echarts.apache.org/zh/option.html#toolbox
+   */
+  public setToolbox(toolbox?: ToolboxComponentOption) {
+    if (!toolbox) {
+      return (this.chartOptions.toolbox = {});
+    }
+    return (this.chartOptions.toolbox = toolbox);
+  }
+
+  /**
+   * Get This Charts xAxis Option
+   * @returns xAxis Options Data
+   */
+  public getXAxis() {
+    return this.chartOptions.xAxis;
+  }
+
+  public setXAxis(axisOption: XAXisComponentOption) {
+    return (this.chartOptions.xAxis = axisOption);
+  }
+  /**
+   * Get This Charts yAxis Option
+   * @returns yAxis Options Data
+   */
+  public getYAxis() {
+    return this.chartOptions.yAxis;
+  }
+
+  public setYAxis(axisOption: YAXisComponentOption) {
+    return (this.chartOptions.yAxis = axisOption);
+  }
+
+  /**
+   * Get This Charts Series Data
+   * @returns Series Data
+   */
+  public getSeriesData() {
+    return this.chartOptions.series;
+  }
+
+  public setSeriesData<T>(seriesData: T) {
+    return (this.chartOptions.series = seriesData);
+  }
+
+  public setTemplateData() {
+    this.chartOptions = {
+      title: {
+        text: 'Sample Charts',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Sample Data'],
+      },
+      grid: {
+        left: '4%',
+        right: '4%',
+        bottom: '4%',
+        containLabel: true,
+      },
+      toolbox: {},
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Sample Data',
+          type: 'line',
+          stack: 'Total',
+          smooth: true,
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+      ],
+    };
+  }
+
+  /**
+   * Render Chart
+   */
   public render() {
-    this.initialChart();
+    if (!this.initChart) {
+      this.initialChart();
+    }
     this.initChart!.setOption(this.chartOptions);
   }
 
-  public resize() {}
+  /**
+   * Resize Charts
+   * @param timeout {number}
+   */
+  public resize(timeout: number = 1000) {
+    clearInterval(Number(this.resizeTimer));
+    this.resizeTimer = setTimeout(() => {
+      this.initChart!.resize();
+    }, timeout);
+  }
 }
 
 export default ECharts;
--- src/views/example/dashboard.tsx
+++ src/views/example/dashboard.tsx
@@ -4,25 +4,54 @@
  * @Author: Mirage
  * @Date: 2022-08-02 16:20:11
  * @LastEditors: Mirage
- * @LastEditTime: 2022-08-19 15:56:55
+ * @LastEditTime: 2022-08-22 17:46:51
  */
 import { defineComponent, onMounted, ref } from 'vue';
 import ECharts from '@/utils/ECharts';
-
-const initial = () => {
-  const charts = document.getElementById('charts');
-  const EChart = new ECharts(charts!);
-  EChart.render();
-};
 
 const Dashboard = defineComponent({
   name: 'Dashboard',
   setup() {
     const charts = ref<HTMLElement>();
+    let objectEChart: ECharts | null = null;
+    let timer: NodeJS.Timer | null | number | undefined = null;
+
     onMounted(() => {
-      initial();
+      const charts = document.getElementById('charts');
+      objectEChart = new ECharts(charts!);
+      objectEChart.setTemplateData();
+      objectEChart.render();
+      window.addEventListener('resize', function () {
+        objectEChart!.resize();
+      });
     });
-    return { charts };
+
+    const setNewData = () => {
+      clearInterval(Number(timer));
+      const random = Math.random();
+      const newData = {
+        name: 'Sample Data',
+        type: 'line',
+        stack: 'Total',
+        smooth: true,
+        data: [
+          random,
+          random * Math.random() * 10,
+          random * Math.random() * 10,
+          random * Math.random() * 10,
+          random * Math.random() * 10,
+          random * Math.random() * 10,
+          random * Math.random() * 10,
+        ],
+      };
+      timer = setInterval(() => {
+        console.log('x');
+        objectEChart?.setSeriesData(newData);
+        objectEChart?.render();
+      }, 1000);
+    };
+
+    return { charts, setNewData };
   },
   render() {
     const renderCircle = () => {
@@ -46,6 +75,7 @@ const Dashboard = defineComponent({
     return (
       <el-row gutter={32}>
         <el-col span={6}>{renderCircle}</el-col>
+        <el-button onClick={() => this.setNewData()}>111</el-button>
         <el-col span={24}>{renderCharts}</el-col>
       </el-row>
     );
