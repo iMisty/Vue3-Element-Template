8a02ec67a50c50ab2cc20715aeb858df3159ba67
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-09-17 00:59:32 +0800

    :zap: Update Statistics Component
    - Perfecting Type and interface

--- src/components/SimpleStatistics/statistics.tsx
+++ src/components/SimpleStatistics/statistics.tsx
@@ -1,21 +1,28 @@
 /*
  * @Author: Miya
  * @Date: 2022-08-23 21:46:17
- * @LastEditTime: 2022-08-23 22:29:07
+ * @LastEditTime: 2022-09-17 00:03:41
  * @LastEditors: Miya
  * @Description: Simple Statistics Component
  * @FilePath: \Vue3-Element-Template\src\components\SimpleStatistics\statistics.tsx
  */
 
 import { FunctionalComponent } from 'vue';
+import type { VNode } from 'vue';
 import Style from './style/statistics.module.less';
 
-interface SimpleStatisticsProps {
-  title: string;
-  sum: string;
-  isSimple: boolean;
-  isRightCenter: boolean;
-}
+export type SimpleStatisticsProps = {
+  title?: string;
+  sum?: string | number;
+  isSimple?: boolean;
+  isRightCenter?: boolean;
+};
+
+export type StatisticsSlot = {
+  default?: () => JSX.Element | VNode;
+  icon?: () => JSX.Element | VNode;
+  chart?: () => JSX.Element | VNode;
+};
 
 const SimpleStatistics: FunctionalComponent<SimpleStatisticsProps> = (
   props: SimpleStatisticsProps,
