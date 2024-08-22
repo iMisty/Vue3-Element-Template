a5a1073fb773e64953dca84cc2a548844a81dd92
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2024-08-22 15:32:47 +0800

    Update: Includes tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,3 @@
-/**
- * @Author: Miya
- * @Date: 2024-08-22 10:44:32
- * @LastEditTime: 2024-08-22 10:44:39
- * @LastEditors: Miya
- * @Description: 
- * @FilePath: \Vue3-Element-Template\postcss.config.js
- */
 module.exports = {
   plugins: {
     tailwindcss: {},
